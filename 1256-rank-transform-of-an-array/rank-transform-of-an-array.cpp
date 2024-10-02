ec39dc53a34144a1482a53db1f0ad14801bf838f
Anurag Negi <123005508+Anurag-Negi28@users.noreply.github.com>
2024-10-02 23:44:10 +0530

    Time: 36 ms (97.78%) | Memory: 36.7 MB (96.30%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
-    vector<int> arrayRankTransform(vector<int>& arr) {
-        // Store the rank for each number in arr.
-        map<int, vector<int>> numToIndices;
-
-        for (int i = 0; i < arr.size(); i++) {
-            numToIndices[arr[i]].push_back(i);
+    using int2=pair<int, int>;
+    static vector<int> arrayRankTransform(vector<int>& arr) {
+        const int n=arr.size();
+        vector<int2> arrIdx(n);
+        for(int i=0; int x: arr){
+            arrIdx[i]={x, i};
+            i++;
         }
-
-        int rank = 1;
-        for (auto& pair : numToIndices) {
-            for (int index : pair.second) {
-                arr[index] = rank;
-            }
-            rank++;
+        sort(arrIdx.begin(), arrIdx.end());
+        vector<int> ans(n);
+        int prev=INT_MIN, curr=0;
+        for(auto& [x, i]: arrIdx){
+            if (x>prev) curr++;
+            ans[i]=curr;
+            prev=x;
         }
-        return arr;
+        return ans;
     }
 };
+
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
