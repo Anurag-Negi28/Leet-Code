8c1a86cf54064de489a5a8c4f4bde4349b34e96d
Anurag Negi <123005508+Anurag-Negi28@users.noreply.github.com>
2024-10-10 21:34:37 +0530

    Time: 67 ms (18.17%) | Memory: 47 MB (37.47%) - LeetSync

--- 1002-maximum-width-ramp/maximum-width-ramp.cpp
+++ 1002-maximum-width-ramp/maximum-width-ramp.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     int maxWidthRamp(vector<int>& nums) {
         int n = nums.size();
-        vector<int> rightMax(n);
+        stack<int> indicesStack;
 
-        // Fill rightMax array with the maximum values from the right
-        rightMax[n - 1] = nums[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            rightMax[i] = max(rightMax[i + 1], nums[i]);
+        // Fill the stack with indices in increasing order of their values
+        for (int i = 0; i < n; i++) {
+            if (indicesStack.empty() || nums[indicesStack.top()] > nums[i]) {
+                indicesStack.push(i);
+            }
         }
 
-        int left = 0, right = 0;
         int maxWidth = 0;
 
-        // Traverse the array using left and right pointers
-        while (right < n) {
-            // Move left pointer forward if current left exceeds rightMax
-            while (left < right && nums[left] > rightMax[right]) {
-                left++;
+        // Traverse the array from the end to the start
+        for (int j = n - 1; j >= 0; j--) {
+            while (!indicesStack.empty() &&
+                   nums[indicesStack.top()] <= nums[j]) {
+                maxWidth = max(maxWidth, j - indicesStack.top());
+                // Pop the index since it's already processed
+                indicesStack.pop();
             }
-            maxWidth = max(maxWidth, right - left);
-            right++;
         }
 
         return maxWidth;
