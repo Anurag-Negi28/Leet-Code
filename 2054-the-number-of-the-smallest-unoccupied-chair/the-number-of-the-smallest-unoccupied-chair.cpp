e9ddcd1078e34f62cc7f97616aff224160f335d6
Anurag Negi <123005508+Anurag-Negi28@users.noreply.github.com>
2024-10-11 23:30:54 +0530

    Time: 139 ms (73.20%) | Memory: 64.9 MB (40.21%) - LeetSync

--- 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.cpp
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.cpp
@@ -1,21 +1,48 @@
 class Solution {
 public:
     int smallestChair(vector<vector<int>>& times, int targetFriend) {
-        vector<int> targetTime = times[targetFriend];
-        sort(times.begin(), times.end());
-
         int n = times.size();
-        vector<int> chairTime(n);
+        vector<pair<int, int>>
+            events;  // To store both arrival and leave events
 
-        for (auto time : times) {
-            for (int i = 0; i < n; i++) {
-                if (chairTime[i] <= time[0]) {
-                    chairTime[i] = time[1];
-                    if (time == targetTime) return i;
-                    break;
-                }
+        // Populate events with {arrival, friendIndex} and {leave, -friendIndex}
+        for (int i = 0; i < n; i++) {
+            events.push_back({times[i][0], i});   // Friend arrives
+            events.push_back({times[i][1], ~i});  // Friend leaves
+        }
+
+        sort(events.begin(), events.end());  // Sort events by time
+
+        priority_queue<int, vector<int>, greater<int>>
+            availableChairs;  // Min-heap for available chairs
+        priority_queue<pair<int, int>, vector<pair<int, int>>,
+                       greater<pair<int, int>>>
+            occupiedChairs;  // Min-heap to track when chairs will be vacated
+
+        for (int i = 0; i < n; i++)
+            availableChairs.push(i);  // Initially all chairs are free
+
+        for (auto& event : events) {
+            int time = event.first;
+            int friendIndex = event.second;
+
+            // Free up chairs when friends leave
+            while (!occupiedChairs.empty() &&
+                   occupiedChairs.top().first <= time) {
+                availableChairs.push(occupiedChairs.top().second);
+                occupiedChairs.pop();
+            }
+
+            if (friendIndex >= 0) {  // Friend arrives
+                int chair = availableChairs.top();
+                availableChairs.pop();
+                if (friendIndex == targetFriend) return chair;
+                occupiedChairs.push(
+                    {times[friendIndex][1],
+                     chair});  // Mark when the chair will be vacated
             }
         }
-        return 0;
+
+        return -1;  // Should never reach here
     }
 };
