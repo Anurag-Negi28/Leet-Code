532e7f28a232377d4111ab9fdf814c1a3f8cefae
Anurag Negi <123005508+Anurag-Negi28@users.noreply.github.com>
2025-04-22 18:38:51 +0530

    Time: 1 ms (5.54%) | Memory: 8.6 MB (45.00%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
+    double myPow(double x, long long n) {
         
         double result=1;
-        result=pow(x,n);
-        /*
+        //result=pow(x,n);
+        bool isNegative=n<0;
+        if(isNegative){
+            n=-n;
+        }
         while(n>0){
             if(n&1){
                 result=result*x;
             }
             x=x*x;
             n=n>>1;
-        }*/
-        return result;
+        }
+        return isNegative? 1.0/result: result;
     }
 };
