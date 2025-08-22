b1249f1600208d5e389c4342554e1987df3ccfac
Anurag Negi <123005508+Anurag-Negi28@users.noreply.github.com>
2025-08-22 23:58:01 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (78.69%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,27 +1,37 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m=matrix.size();
-        int n=matrix[0].size();
-        vector<pair<int,int>> position;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        //No. of rows
+        int n=matrix.size();
+        //No. of columns
+        int m=matrix[0].size();
+        int col0=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
-                    position.push_back({i,j});
+                    matrix[i][0]=0;
+                    if(j!=0)
+                    matrix[0][j]=0;
+                    else
+                    col0=0;
                 }
             }
         }
-        //Make the row zero
-        for(int i=0;i<position.size();i++){
-            for(int j=0;j<n;j++){
-                matrix[position[i].first][j]=0;
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][j]!=0){
+                    if(matrix[0][j]==0 || matrix[i][0]==0) 
+                    matrix[i][j]=0; 
+                }
             }
         }
-        //Make the column zero
-        for(int i=0;i<position.size();i++){
-            for(int j=0;j<m;j++){
-                matrix[j][position[i].second]=0;
-            }
+
+        for(int j=0;j<m;j++){
+            if(matrix[0][0]==0) matrix[0][j]=0;
+        }
+        for(int i=0;i<n;i++){ 
+            if(col0==0) matrix[i][0]=0;
         }
     }
 };
