e7d1a8d3470d5bcd0395c1788336b04be6662ff4
Anurag Negi <123005508+Anurag-Negi28@users.noreply.github.com>
2025-08-24 18:58:08 +0530

    Time: 0 ms (100.00%) | Memory: 65.1 MB (69.97%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -3,11 +3,14 @@ public:
     int findDuplicate(vector<int>& nums) {
         int slow=nums[0];
         int fast=nums[0];
+        //Phase1-Detect the cycle by moving slow 1 step and fast 2step a time
         do{
             slow=nums[slow];
             fast=nums[nums[fast]];
         }while(slow!=fast);
+        //Phase 2-Once the cycle is detected change fast to start
         fast=nums[0];
+        //Phase 3-Then move slow and fast 1 step a time and the point where the collide is the duplicate
         while(slow!=fast){
             slow=nums[slow];
             fast=nums[fast];
