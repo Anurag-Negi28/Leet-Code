3bf7e45c801d428e12f4c7c8fea65d40015a2f62
Anurag Negi <123005508+Anurag-Negi28@users.noreply.github.com>
2025-08-30 22:09:15 +0530

    Time: 17 ms (6.59%) | Memory: 27.1 MB (5.54%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -4,11 +4,11 @@ public:
         vector<int> res;
         map<int,int> mp;
         int n=nums.size();
+        int count=(int)(n/3)+1;
         for(int i=0;i<n;i++){
             mp[nums[i]]++;
-        }
-        for(auto& p:mp){
-            if(p.second>(n/3)) res.push_back(p.first);
+            if(mp[nums[i]]==count) res.push_back(nums[i]);
+            if(res.size()==2) break;
         }
         return res;
     }
